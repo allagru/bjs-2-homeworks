3e26a811c2477f511940b836912bab327950dd3c
Alla <liliya86@inbox.ru>
2023-04-16 19:58:50 +0300

    add 4 homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,10 +3,7 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    if (users.length === 0) {
-        return 0;
-    }
-    let result = users.filter(user => user.gender === gender)
+    const result = users.filter(user => user.gender === gender)
         .map(user => user.age)
         .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
     return result;
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks && this.marks.length !== 0) {
+        return this.marks.reduce((acc, mark) => acc + mark / this.marks.length, 0);
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
+
+let student1 = new Student('Roza', 'female', 22);
+let student2 = new Student('Roman', 'male', 23);
