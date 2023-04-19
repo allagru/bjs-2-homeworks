dce1bd6f6444460329a06dac72c36d37d9da8b1f
Alla <liliya86@inbox.ru>
2023-04-19 21:01:17 +0300

    add 6 homework

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+    const result = Number.parseFloat(value);
+    if (Number.isNaN(result)) {
+        throw new Error('Невалидное значение');
+    }
+    return result;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        if (sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    get perimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    get area() {
+        const semiPerimeter = this.perimeter / 2;
+        const square = Math.sqrt(semiPerimeter * (semiPerimeter - this.sideA) * (semiPerimeter - this.sideB) * (semiPerimeter - this.sideC));
+        return +square.toFixed(3);
+    }
+}
+
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
