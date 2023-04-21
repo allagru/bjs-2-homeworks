c7697e39a25a00d5ce94982824702c7c025f2cc5
Alla <liliya86@inbox.ru>
2023-04-22 00:21:13 +0300

    add 7 homework

---
+++ 7.async/task.js
@@ -0,0 +1,61 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (time === null || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        if (this.alarmCollection.includes(time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+        this.alarmCollection.push({
+            callback,
+            time,
+            canCall: true
+        })
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        const timeCurrent = new Date().toLocaleTimeString('ru-Ru', { hour: '2-digit', minute: '2-digit' });
+        return timeCurrent;
+    }
+
+    start() {
+        if (this.intervalId !== null) {
+            return;
+        }
+
+        function createInterval() {
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall === true) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            })
+        }
+
+        const bindedFunc = createInterval.bind(this);
+        this.intervalId = setInterval(bindedFunc);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
